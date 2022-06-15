de11f5f13ac7edf4b006b148195bccc506f6fef0
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:34:30 -0300

    Add files via upload

---
+++ 21-NumeroCuadradoCubo/21-NumeroCuadradoCubo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _21_NumeroCuadradoCubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, numero, cuadrado, cubo;
+
+            Console.WriteLine("Ingrese un numero para desarrollar su tabla o presione cero para cerrar el programa: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n != 0)
+            {
+
+
+                numero = n;
+                cuadrado = n * n;
+                cubo = n * n * n;
+
+                Console.WriteLine("En Numero es: " + numero);
+                Console.WriteLine("El Cuadrado es: " + cuadrado);
+                Console.WriteLine("El Cubo es: " + cubo);
+
+                Console.WriteLine("Ingrese un numero para desarrollar su tabla o presione cero para cerrar el programa: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
